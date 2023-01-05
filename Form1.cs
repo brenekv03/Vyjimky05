79b2cf6d1b337583cff29bc5721d23eca69c220f
brenekv.03 <brenekv.03@A321-15>
2023-01-05 11:01:25 +0100

    Přidejte soubory projektu.

---
+++ Form1.cs
@@ -0,0 +1,62 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Vyjimky05
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            int[] pole = new int[textBox1.Lines.Length];
+            for(int i = 0; i < textBox1.Lines.Length; i++)
+            {
+                try
+                {
+                    int cislo = int.Parse(textBox1.Lines[i]);
+                    try
+                    {
+                        int dvojnasobek = checked(cislo * 2);
+                        pole[i] = dvojnasobek;
+                    }
+                    catch(OverflowException)
+                    {
+                        pole[i] = cislo;
+                    }
+                }
+                catch(FormatException)
+                {
+                    pole[i] = 0;
+                }
+                catch(OverflowException)
+                {
+                    string text = textBox1.Lines[i];
+                    if (text[0] == '-')
+                    {
+                        pole[i]=Int32.MinValue;
+                    }
+                    else
+                    {
+                        pole[i] = Int32.MaxValue;
+                    }
+                }
+            }
+            listBox1.Items.Clear();
+            foreach(int x in pole)
+            {
+                listBox1.Items.Add(x);
+            }
+        }
+    }
+}
